2384a74a8b4243efa39613cb7d7a6e41649620cd
anniebaker <bakeranneabb@gmail.com>
2024-08-14 11:38:12 -0500

    Grid setup

--- build-a-board/src/App.tsx
+++ build-a-board/src/App.tsx
@@ -1,7 +1,23 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  const layout = { base: `"nav" "main"`, md: `"nav nav" "aside main"` };
+  return (
+    <Grid templateAreas={layout}>
+      <GridItem area={"nav"} bg="papayawhip">
+        Nav
+      </GridItem>
+      <Show above="md">
+        <GridItem area={"aside"} bg="tomato">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg="azure">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
